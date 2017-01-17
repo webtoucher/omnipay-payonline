184cf9594464c435e3b882500e80c922e69054a3
Alexey Kuznetsov <mirakuru@webtoucher.ru>
2017-01-17 23:57:11 +0300

    Add french and german localization

---
+++ src/Exception/de/HumanError.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Omnipay\PayOnline\Exception\de;
+
+use Omnipay\PayOnline\Exception\LocalizedError;
+
+/**
+ * PayOnline protocol errors german localization.
+ */
+class HumanError extends LocalizedError
+{
+    protected static $_errors = [
+    ];
+}
---
+++ src/Exception/fr/HumanError.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Omnipay\PayOnline\Exception\fr;
+
+use Omnipay\PayOnline\Exception\LocalizedError;
+
+/**
+ * PayOnline protocol errors french localization.
+ */
+class HumanError extends LocalizedError
+{
+    protected static $_errors = [
+    ];
+}
